9580d517a0922cc92082c58d592691111fd75eb7
HanadiAbouHamdan <hanadi.abouhamdan97@hotmail.com>
2023-02-23 22:31:23 +0200

    Adding the number of scores and organizing the win step

--- MazeGame/game.js
+++ MazeGame/game.js
@@ -1,20 +1,54 @@
 
+ 
+ document.addEventListener("DOMContentLoaded",function(event){
+ 
+ 
  var begin=document.getElementById('start');
  var boundaries=document.getElementsByClassName('boundary');
+var msg=document.getElementById('status');
 var final=document.getElementById('end');
+var count=0;
 var score=0;
-for(let i=0;i<boundaries.length;i++){
-    boundaries[i].addEventListener('mouseover',()=>{
-        boundaries[i].style.border='1px solid red';
-        const msg=document.getElementById('status');
-        msg.textContent='You Lose';
-    });
-}
 
-begin.addEventListener('click',()=>{
-    for (let i=0; i<boundaries.length;i++){
-        boundaries[i].style.border='1px solid black';
-        const msg=document.getElementById('status');
-        msg.textContent='You have another Chance';
+
+
+begin.addEventListener('click', function() {
+    for (let i = 0; i < boundaries.length; i++) {
+        boundaries[i].style.border = '1px solid black';
     }
+    score=0;
+    count=0;
+    msg.textContent = 'Your score is' +score;
+    
+});
+
+begin.addEventListener('mouseover', function() {
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].style.border = '1px solid black';
+    }
+    msg.textContent = 'Your score is' +score;
+    
+});
+
+for (var i = 0; i < boundaries.length; i++) {
+    boundaries[i].addEventListener('mouseover', function() {
+        for (var j = 0; j < boundaries.length; j++) {
+        boundaries[j].style.border = '1px solid red';}
+        var msg = document.getElementById('status');
+        score -= 10;
+        msg.textContent = 'You Lose! Your score is' +score;
+        count++;
+        console.log(count);
+        if(count>5){
+        msg.textContent='No REMAIN CHANCES!';
+        }
+    });
+    }
+
+final.addEventListener('mouseover',function(b){
+if (count==0){
+    score+=5;
+    msg.textContent='You win!Your score is'+score;
+}
 })
+ })
